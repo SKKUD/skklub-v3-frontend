5a324596b7569906450fe59c3e351c72d5acb432
daniel2231 <kangheon2231@gmail.com>
2021-08-27 03:48:33 +0900

    init

---
+++ components/common/navbar/RightMenu.jsx
@@ -0,0 +1,19 @@
+import { Menu, Icon } from "antd";
+
+const SubMenu = Menu.SubMenu;
+const MenuItemGroup = Menu.ItemGroup;
+
+function RightMenu() {
+  return (
+    <Menu mode="horizontal">
+      <Menu.Item key="mail">
+        <a href="">Signin</a>
+      </Menu.Item>
+      <Menu.Item key="app">
+        <a href="">Signup</a>
+      </Menu.Item>
+    </Menu>
+  );
+}
+
+export default RightMenu;
