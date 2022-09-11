edc111842686af02737fbd35b62897b6d85ff547
Daniel2231 <daniel2231.dev@gmail.com>
2022-09-12 01:12:13 +0900

    Changed compiler to SWC, added and removed packages

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,10 @@
 import Head from 'next/head'
 import Image from 'next/image'
-import styles from '../styles/Home.module.css'
-import StyledButton from "../components/common/button"
 
 export default function Home() {
   return (
     <div>
-      <StyledButton />
+      <h1>Testing Nextjs</h1>
     </div>
   )
 }
