a0ed37cfb9af6a49a1f25b752c8b698964e310e6
Daniel2231 <daniel2231.dev@gmail.com>
2023-02-26 18:03:01 +0900

    Updated build.sh

--- build.sh
+++ build.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 cd ../
+npm run build
 mkdir output
-cp -R ./skklub-v3-frontend/* ./output
-cp -R ./output ./skklub-v3-frontend/
+cp -R ./skklub-v3-frontend/.next ./output
+cp -R ./output ./skklub-v3-frontend/.next
