dc8827995c04837ec089b181295fae3ccb499002
Haeun Oh <73781124+OHaEuN@users.noreply.github.com>
2023-02-28 15:36:40 +0900

    Create DepartmentCard

---
+++ components/intro/DepartmentCard.jsx
@@ -0,0 +1,5 @@
+import { Card } from "@mui/material";
+
+export default function DepartmentCard({ dy }) {
+  return <Card>{dy.title}</Card>;
+}
--- layout/intro/IntroLayout.js
+++ layout/intro/IntroLayout.js
@@ -1,7 +1,27 @@
+import DepartmentCard from "../../components/intro/DepartmentCard";
+
+const dongyeon = {
+  title: "for:동",
+  section: ["Who we are 내용", "What we do 내용", "How we work 내용"],
+  department: [
+    {
+      name: "사무국",
+      emoji: "✏️",
+      description: "부서 소개 내용 .... ",
+    },
+    {
+      name: "홍보국",
+      emoji: "🔊",
+      description: "부서 소개 내용 .... ",
+    },
+  ],
+};
+
 export default function IntroLayout(props) {
-    return (
-        <div>
-        <h1>This is {props.campus}</h1>
-        </div>
-    )
+  return (
+    <div>
+      <h1>This is {props.campus}</h1>
+      <DepartmentCard dy={dongyeon} />
+    </div>
+  );
 }
