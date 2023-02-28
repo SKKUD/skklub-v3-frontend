9e806bd3b6a9bdb44604abd409ae7cfdd5239603
Daniel2231 <daniel2231.dev@gmail.com>
2023-02-28 16:15:19 +0900

    added intro layout

--- layout/intro/IntroLayout.js
+++ layout/intro/IntroLayout.js
@@ -1,11 +1,21 @@
 import { useTheme } from '@emotion/react';
-import { Box } from '@mui/system';
+import styled from '@emotion/styled';
+
+const LayoutBackground = styled.div`
+	background-color: ${(props) => props.theme.palette.background.default};
+	color: ${(props) => props.theme.palette.text.primary};
+	min-height: 100vh;
+	display: flex;
+	flex-direction: column;
+	justify-content: center;
+	align-items: center;
+`;
 
 export default function IntroLayout(props) {
 	const theme = useTheme();
 	return (
-		<div>
-			<h1>This is {props.campus}</h1>
-		</div>
+        <LayoutBackground>
+            
+		</LayoutBackground>
 	);
 }
