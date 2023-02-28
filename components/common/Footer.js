c393e93afbf318fe21dbdf3299f3f7563986c49d
Haeun Oh <73781124+OHaEuN@users.noreply.github.com>
2023-02-28 22:51:02 +0900

    fix footer

--- components/common/Footer.js
+++ components/common/Footer.js
@@ -3,13 +3,21 @@ import styled from "@emotion/styled";
 const FooterWrap = styled.div`
   width: 100%;
   height: 300px;
-  color: #fff;
-  text-align: center;
-  line-height: 300px;
   font-size: 20px;
+  color: #fff;
   font-weight: 600;
+  text-align: center;
   background-color: #262626;
   margin-top: 70px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  word-break: keep-all;
+
+  @media (max-width: 768px) {
+    height: 200px;
+    font-size: 14px;
+  }
 `;
 
 export default function Footer() {
