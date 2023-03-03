99846dbbc91fb6cb24968461a3abc65c5149ad14
Haeun Oh <73781124+OHaEuN@users.noreply.github.com>
2023-03-03 19:29:52 +0900

    commit to checkout

--- .storybook/main.js
+++ .storybook/main.js
@@ -2,7 +2,7 @@ module.exports = {
   stories: [
     "../stories/**/*.stories.mdx",
     "../stories/**/*.stories.@(js|jsx|ts|tsx)",
-    "../components/**/*.stories.@(js|jsx|ts|tsx)",
+    "../components/**/**/*.stories.js",
   ],
   addons: [
     "@storybook/addon-links",
@@ -13,4 +13,29 @@ module.exports = {
   core: {
     builder: "@storybook/builder-webpack5",
   },
+  webpackFinal: async (config) => {
+    return merge(config, {
+      resolve: {
+        alias: {
+          "@emotion/react": getPackageDir("@emotion/react"),
+        },
+      },
+    });
+  },
 };
+
+function getPackageDir(filepath) {
+  let currDir = path.dirname(require.resolve(filepath));
+  while (true) {
+    if (fs.existsSync(path.join(currDir, "package.json"))) {
+      return currDir;
+    }
+    const { dir, root } = path.parse(currDir);
+    if (dir === root) {
+      throw new Error(
+        `Could not find package.json in the parent directories starting from ${filepath}.`
+      );
+    }
+    currDir = dir;
+  }
+}
