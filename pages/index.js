a14ab64eff62070f4e79982d4bccfc67f0bb014e
richcire <eric9907@naver.com>
2023-04-09 21:27:30 +0900

    build basic homepage layout

--- pages/index.js
+++ pages/index.js
@@ -2,11 +2,47 @@ import Head from "next/head";
 import Image from "next/image";
 import Footer from "../components/common/Footer";
 import IntroLayout from "../layout/intro/IntroLayout";
+import styled from "@emotion/styled";
+
+const HomePageContainer = styled.div`
+  width: 100vw;
+  height: 100vh;
+  position: relative;
+`;
+
+const ItemContainer = styled.div`
+  position: absolute;
+  left: 50%;
+  top: 50%;
+  transform: translate(-50%, -30%);
+`;
+
+const Logo = styled.div`
+  width: 100px;
+  height: 100px;
+  background-color: beige;
+`;
+
+const PhraseBtnContainer = styled.div``;
+
+const Phrase = styled.div``;
+
+const BtnContainer = styled.div`
+  width: 100px;
+  height: 100px;
+  background-color: blue;
+`;
 
 export default function Home() {
   return (
-    <div>
-      <h1>This is home</h1>
-    </div>
+    <HomePageContainer>
+      <ItemContainer>
+        <Logo></Logo>
+        <PhraseBtnContainer>
+          <Phrase></Phrase>
+          <LocationBtnContainer />
+        </PhraseBtnContainer>
+      </ItemContainer>
+    </HomePageContainer>
   );
 }
