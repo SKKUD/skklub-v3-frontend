718d912bcc6af058bf6da9df3d7100240dec4bf2
richcire <eric9907@naver.com>
2023-04-10 21:59:53 +0900

    make notice content

--- pages/seoul/index.js
+++ pages/seoul/index.js
@@ -2,6 +2,9 @@ import styled from "@emotion/styled";
 
 import Head from "next/head";
 import Image from "next/image";
+import Carousel from "react-material-ui-carousel";
+import ArrowBackIosNewIcon from "@mui/icons-material/ArrowBackIosNew";
+import ArrowForwardIosIcon from "@mui/icons-material/ArrowForwardIos";
 
 // const CLUB_RECCOMANDATION_DUMMY_DATA = [{}]
 
@@ -30,13 +33,15 @@ const PromotionBanner = styled.div`
 const ContentContainer = styled.div`
   width: 100%;
   height: 1440px;
-  max-width: 1120px;
+  max-width: 1300px;
   margin: 0 auto;
-  background-color: grey;
+  /* background-color: grey; */
 `;
 
 const RecommendationContent = styled.div`
-  width: 100%;
+  width: 1200px;
+  padding: 0px 20px;
+  margin: 0 auto;
   margin-top: 120px;
 `;
 
@@ -50,6 +55,7 @@ const CircleContainer = styled.div`
   width: 100%;
   gap: 17px;
   margin-top: 24px;
+  padding: 0px 30px;
 `;
 
 const IntroCircle = styled.div`
@@ -112,7 +118,99 @@ const ClubTitle = styled.div`
   font-weight: 500;
 `;
 
-const NoticeContent = styled.div``;
+const NoticeContent = styled.div`
+  margin-top: 10rem;
+  position: relative;
+`;
+
+const MoreBtn = styled.button`
+  background-color: #4e5353;
+  width: 104px;
+  height: 33px;
+  font-weight: 400;
+  font-size: 1.25rem;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  position: absolute;
+  top: 0;
+  right: 120px;
+  border: none;
+  border-radius: 104px;
+  color: white;
+`;
+
+const CarouselContainer = styled.div`
+  height: 346px;
+  width: 1200px;
+  background-color: transparent;
+  margin: 0 auto;
+  margin-top: 2rem;
+`;
+
+const DefaultSettingsT = {
+  autoPlay: true,
+  animation: "slide",
+  indicators: false,
+  duration: 500,
+  navButtonsAlwaysVisible: true,
+  navButtonsAlwaysInvisible: false,
+  cycleNavigation: true,
+  fullHeightHover: true,
+  swipe: true,
+};
+
+const CardRow = styled.div`
+  height: 346px;
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  gap: 28px;
+`;
+
+const ClubCard = styled.div`
+  width: 244px;
+  height: 346px;
+  /* background-image: url(${(props) => props.imageUrl}); */
+  background-repeat: no-repeat;
+  background-size: cover;
+  background: linear-gradient(rgba(0, 0, 0, 0.3), rgba(0, 0, 0, 0.3)),
+    url(${(props) => props.imageUrl});
+  border-radius: 1rem;
+  box-shadow: inset 0 1em 40px rgba(0, 0, 0, 0.5),
+    inset 0 -1em 40px rgba(0, 0, 0, 0.5);
+  position: relative;
+  display: flex;
+  flex-direction: column;
+  justify-content: end;
+  padding-left: 0.75rem;
+  align-items: start;
+`;
+
+const CardDate = styled.div`
+  position: absolute;
+  top: 14px;
+  right: 15px;
+  font-weight: 400;
+  font-size: 1rem;
+  color: white;
+`;
+
+//제목이 길경우 짧게 파싱해주어야함
+const CardTitle = styled.div`
+  color: white;
+  font-weight: 600;
+  font-size: 1.25rem;
+  width: 224px;
+  margin-bottom: 0.75rem;
+`;
+
+const CardClubName = styled.div`
+  font-weight: 400;
+  font-size: 1rem;
+  color: white;
+  margin-bottom: 17px;
+`;
 
 const BorrowContent = styled.div``;
 
@@ -148,6 +246,88 @@ export default function Seoul() {
             </ClubCircle>
           </CircleContainer>
         </RecommendationContent>
+        <NoticeContent>
+          <Phrase style={{ marginLeft: "90px" }}>공지사항</Phrase>
+          <MoreBtn>MORE</MoreBtn>
+          <CarouselContainer>
+            <Carousel
+              {...DefaultSettingsT}
+              NextIcon={<ArrowForwardIosIcon />}
+              PrevIcon={<ArrowBackIosNewIcon />}
+              fullHeightHover={false}
+            >
+              <CardRow>
+                <ClubCard imageUrl="/assets/images/one.png">
+                  <CardDate>JAN 9, 2021</CardDate>
+                  <CardTitle>하반기 수선관 운동장 예약어쩌구</CardTitle>
+                </ClubCard>
+                <ClubCard imageUrl="/assets/images/two.png">
+                  <CardDate>JAN 9, 2021</CardDate>
+                  <CardTitle>
+                    [스포츠] 산악 바이크 취미로 타실 분들 모집합니다.
+                  </CardTitle>
+                  <CardClubName>스꾸모터</CardClubName>
+                </ClubCard>
+                <ClubCard imageUrl="/assets/images/three.png">
+                  <CardDate>JAN 9, 2021</CardDate>
+                  <CardTitle>[스포츠] 성균관배 골프대회 개최안내</CardTitle>
+                  <CardClubName>성골퍼</CardClubName>
+                </ClubCard>
+                <ClubCard imageUrl="/assets/images/four.png">
+                  <CardDate>JAN 9, 2021</CardDate>
+                  <CardTitle>[밴드] 제 22회 초청공연</CardTitle>
+                  <CardClubName>못갖춘 마디</CardClubName>
+                </ClubCard>
+              </CardRow>
+              <CardRow>
+                <ClubCard imageUrl="/assets/images/one.png">
+                  <CardDate>JAN 9, 2021</CardDate>
+                  <CardTitle>하반기 수선관 운동장 예약어쩌구</CardTitle>
+                </ClubCard>
+                <ClubCard imageUrl="/assets/images/two.png">
+                  <CardDate>JAN 9, 2021</CardDate>
+                  <CardTitle>
+                    [스포츠] 산악 바이크 취미로 타실 분들 모집합니다.
+                  </CardTitle>
+                  <CardClubName>스꾸모터</CardClubName>
+                </ClubCard>
+                <ClubCard imageUrl="/assets/images/three.png">
+                  <CardDate>JAN 9, 2021</CardDate>
+                  <CardTitle>[스포츠] 성균관배 골프대회 개최안내</CardTitle>
+                  <CardClubName>성골퍼</CardClubName>
+                </ClubCard>
+                <ClubCard imageUrl="/assets/images/four.png">
+                  <CardDate>JAN 9, 2021</CardDate>
+                  <CardTitle>[밴드] 제 22회 초청공연</CardTitle>
+                  <CardClubName>못갖춘 마디</CardClubName>
+                </ClubCard>
+              </CardRow>
+              <CardRow>
+                <ClubCard imageUrl="/assets/images/one.png">
+                  <CardDate>JAN 9, 2021</CardDate>
+                  <CardTitle>하반기 수선관 운동장 예약어쩌구</CardTitle>
+                </ClubCard>
+                <ClubCard imageUrl="/assets/images/two.png">
+                  <CardDate>JAN 9, 2021</CardDate>
+                  <CardTitle>
+                    [스포츠] 산악 바이크 취미로 타실 분들 모집합니다.
+                  </CardTitle>
+                  <CardClubName>스꾸모터</CardClubName>
+                </ClubCard>
+                <ClubCard imageUrl="/assets/images/three.png">
+                  <CardDate>JAN 9, 2021</CardDate>
+                  <CardTitle>[스포츠] 성균관배 골프대회 개최안내</CardTitle>
+                  <CardClubName>성골퍼</CardClubName>
+                </ClubCard>
+                <ClubCard imageUrl="/assets/images/four.png">
+                  <CardDate>JAN 9, 2021</CardDate>
+                  <CardTitle>[밴드] 제 22회 초청공연</CardTitle>
+                  <CardClubName>못갖춘 마디</CardClubName>
+                </ClubCard>
+              </CardRow>
+            </Carousel>
+          </CarouselContainer>
+        </NoticeContent>
       </ContentContainer>
     </>
   );
