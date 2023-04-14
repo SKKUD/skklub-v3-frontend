dd477b3efdb200016d16fbc3e2b0ae05651fbb33
richcire <eric9907@naver.com>
2023-04-14 23:05:28 +0900

    make grid layout

--- pages/club-detail.js
+++ pages/club-detail.js
@@ -6,6 +6,52 @@ const ClubDetailBanner = styled.div`
   background-color: #a0211d33;
 `;
 
+const ClubDetailContent = styled.div`
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  max-width: 1200px;
+  margin: 0 auto;
+  margin-top: 3rem;
+  gap: 30px;
+  margin-bottom: 300px;
+`;
+
+const ClubDetailCard = styled.div`
+  background-color: #2c2c2c;
+  height: 264px;
+  border-radius: 10px;
+`;
+
 export default function ClubDetail() {
-  return <ClubDetailBanner></ClubDetailBanner>;
+  return (
+    <>
+      <ClubDetailBanner></ClubDetailBanner>
+      <ClubDetailContent>
+        <ClubDetailCard
+          style={{
+            gridColumnStart: 1,
+            gridColumnEnd: 4,
+          }}
+        ></ClubDetailCard>
+        <ClubDetailCard
+          style={{
+            height: "403px",
+          }}
+        ></ClubDetailCard>
+        <ClubDetailCard
+          style={{
+            gridColumnStart: 2,
+            gridColumnEnd: 4,
+            height: "403px",
+          }}
+        ></ClubDetailCard>
+        <ClubDetailCard
+          style={{
+            gridColumnStart: 1,
+            gridColumnEnd: 4,
+          }}
+        ></ClubDetailCard>
+      </ClubDetailContent>
+    </>
+  );
 }
