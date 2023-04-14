e59b79c0e3482b7e18b22d596f3c78b83bf4e13d
richcire <eric9907@naver.com>
2023-04-14 22:52:20 +0900

    make new branch

---
+++ pages/club-detail.js
@@ -0,0 +1,11 @@
+import styled from "@emotion/styled";
+
+const ClubDetailBanner = styled.div`
+  width: 100vw;
+  height: 378px;
+  background-color: #a0211d33;
+`;
+
+export default function ClubDetail() {
+  return <ClubDetailBanner></ClubDetailBanner>;
+}
