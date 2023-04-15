8c99a724752f0c3e55c10db6baeea21a6143ec4e
richcire <eric9907@naver.com>
2023-04-15 15:49:29 +0900

    make about card

--- pages/club-detail.js
+++ pages/club-detail.js
@@ -23,6 +23,7 @@ const ClubDetailCard = styled.div`
   padding-top: 1.75rem;
   padding-left: 2.125rem;
   padding-right: 2.125rem;
+  position: relative;
 `;
 
 const ClubDetailCardTitle = styled.div`
@@ -80,13 +81,36 @@ const InfoContentRow = styled.div`
 const InfoCategory = styled.div`
   font-weight: 700;
   font-size: 1rem;
-  /* line-height: 160%; */
 `;
 
 const Info = styled.div`
   font-weight: 300;
   font-size: 1rem;
-  /* line-height: 107.5%; */
+`;
+
+const AboutContent = styled.div`
+  font-weight: 300;
+  font-size: 1rem;
+  line-height: 169%;
+  height: 226px;
+`;
+
+const ClubPageNaviagateBtn = styled.button`
+  position: absolute;
+  bottom: 24px;
+  right: 30px;
+  width: 174px;
+  height: 51px;
+  background-color: #c4cbcd;
+  border-radius: 100px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-weight: 700;
+  font-size: 1.25rem;
+  line-height: 107.5%;
+  color: white;
+  border: none;
 `;
 
 export default function ClubDetail() {
@@ -169,7 +193,21 @@ export default function ClubDetail() {
             gridColumnEnd: 4,
             height: "403px",
           }}
-        ></ClubDetailCard>
+        >
+          <ClubDetailCardTitle>About</ClubDetailCardTitle>
+          <ClubDetailCardHr />
+          <AboutContent>
+            성균합창단은 54년의 전통을 잇는 중앙 합창 동아리입니다. 매주 화,
+            목요일 7시 정기 연습과 뒤풀이로 단원간 화합을 도모하고, 매 학기 정기
+            공연을 실시합니다. 가요, 뮤지컬 넘버, 영화 OST 등 다양한 장르로
+            스테이지를 꾸미며, 원하는 스테이지에만 참여할 수 있어 비교적 부담이
+            적습니다. 명륜과 율전을 오가며 다양한 연령대와 학과의 사람을 만나
+            친목을 다질 수 있고, 체육대회, MT 등 여러 행사를 함께합니다. 뛰어난
+            노래 실력, 합창 또는 노래 관련 경험이 없어도, 학번이 높아도
+            괜찮습니다. 편한 마음으로 가볍게 놀러오세요!
+          </AboutContent>
+          <ClubPageNaviagateBtn>동아리 페이지</ClubPageNaviagateBtn>
+        </ClubDetailCard>
         <ClubDetailCard
           style={{
             gridColumnStart: 1,
