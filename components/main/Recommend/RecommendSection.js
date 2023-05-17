4a55c0c3f2192f492c91760ae2b18d25b54d69bb
Haeun Oh <73781124+OHaEuN@users.noreply.github.com>
2023-05-17 19:27:50 +0900

    fix merge error

--- components/main/Recommend/RecommendSection.js
+++ components/main/Recommend/RecommendSection.js
@@ -5,7 +5,6 @@ import RecommendTheme from "./RecommendTheme";
 import RecommendSubTheme from "./RecommendSubThem";
 import Hashtag from "./Hashtag";
 import HashtagWrapper from "./HashtagWrapper";
-import ClubCircle from "./ClubCircle";
 import useCampusDetect from "../../../hooks/useCampusDetect";
 
 const RecommendWrapper = styled.div`
@@ -39,7 +38,6 @@ const IntroCircle = styled.div`
   padding-left: 68px;
 `;
 
-
 const ClubCircle = styled.div`
   width: 153px;
   height: 153px;
@@ -66,7 +64,7 @@ export default function RecommendSection() {
   const { isSuwon } = useCampusDetect();
   const matches_1024 = useMediaQuery("(max-width:1024px)");
   const matches_950 = useMediaQuery("(max-width:950px)");
-  const { isSuwon } = useCampusDetect();
+
   return (
     <RecommendWrapper>
       <Phrase>오늘의 추천동아리</Phrase>
@@ -91,7 +89,6 @@ export default function RecommendSection() {
         </ClubCircle>
         {!matches_1024 && (
           <ClubCircle campus={!isSuwon}>
-
             <ClubTitle>성균 민속 연구반 탈</ClubTitle>
           </ClubCircle>
         )}
