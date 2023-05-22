d836a0558a53cbb3d357d7a6aa590fe41c46ff5d
richcire <eric9907@naver.com>
2023-05-22 22:01:01 +0900

    make club dummy data

---
+++ utils/constants.js
@@ -0,0 +1,46 @@
+export const CLUBS_DUMMY_DATA = [
+  {
+    clubName: "꾼",
+    categories: ["평면예술", "서예"],
+  },
+  {
+    clubName: "별누리",
+    categories: ["평면예술", "서예"],
+  },
+  {
+    clubName: "라켓챌린지",
+    categories: ["평면예술", "서예"],
+  },
+  {
+    clubName: "UBF",
+    categories: ["평면예술", "서예"],
+  },
+  {
+    clubName: "심산한누리",
+    categories: ["평면예술", "서예"],
+  },
+  {
+    clubName: "BENAY",
+    categories: ["평면예술", "서예"],
+  },
+  {
+    clubName: "도서관자치위원회",
+    categories: ["평면예술", "서예"],
+  },
+  {
+    clubName: "성균민속연구반탈",
+    categories: ["평면예술", "서예"],
+  },
+  {
+    clubName: "성균 민속연구반 탈",
+    categories: ["평면예술", "서예"],
+  },
+  {
+    clubName: "빌보드",
+    categories: ["평면예술", "서예"],
+  },
+  {
+    clubName: "스꾸디",
+    categories: ["평면예술", "서예"],
+  },
+];
