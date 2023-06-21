717b8afafb343082e035b4256c2545fee9e9e221
richcire <eric9907@naver.com>
2023-06-21 14:57:24 +0900

    make cardBoard route groups

---
+++ app/seoul/(cardBoard)/layout.js
@@ -0,0 +1,23 @@
+import CampusSwitch from "@/app/components/common/CampusSwitch";
+import Categories from "@/app/components/common/Categories";
+import UpperBanner from "@/app/components/common/UpperBanner";
+import styled from "@emotion/styled";
+
+const MainSection = styled.div`
+  padding-left: 1rem;
+  padding-right: 1rem;
+  padding-bottom: 200px;
+`;
+
+export default function CardBoardLayout({ children }) {
+  return (
+    <>
+      <UpperBanner />
+      <MainSection>
+        <Categories />
+        <CardBoard cardsData={cardsData} />
+      </MainSection>
+      <CampusSwitch />
+    </>
+  );
+}
