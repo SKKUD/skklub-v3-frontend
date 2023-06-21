7d5d15b2c33aa7e27531f009f6c9299611f3bb04
richcire <eric9907@naver.com>
2023-06-21 15:32:23 +0900

    delete useless code

--- hooks/useURLParse.js
+++ hooks/useURLParse.js
@@ -1,6 +1,5 @@
 "use client";
 import { usePathname } from "next/navigation";
-import { useState, useEffect, useCallback } from "react";
 
 const useURLParse = () => {
   const pathName = usePathname();
