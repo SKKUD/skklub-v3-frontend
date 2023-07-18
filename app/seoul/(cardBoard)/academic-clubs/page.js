01361bae10370689d2490ffc52d9718d1da2e1d5
richcire <eric9907@naver.com>
2023-07-18 21:46:40 +0900

    uncomment

--- app/seoul/(cardBoard)/academic-clubs/page.js
+++ app/seoul/(cardBoard)/academic-clubs/page.js
@@ -8,11 +8,11 @@ import { useQuery } from "@tanstack/react-query";
 export default function AcademicClubs() {
   //API 함수호출
   const CARDS_DATA = CLUBS_DUMMY_DATA;
-  // const { data } = useQuery({
-  //   queryKey: ["seoul-academic"],
-  //   queryFn: () => getClubs("명륜", "학회"),
-  //   onSuccess: (data) => console.log(data),
-  //   onError: (error) => console.log(error),
-  // });
+  const { data } = useQuery({
+    queryKey: ["seoul-academic"],
+    queryFn: () => getClubs("명륜", "학회"),
+    onSuccess: (data) => console.log(data),
+    onError: (error) => console.log(error),
+  });
   return <CardBoard cardsData={CARDS_DATA} />;
 }
