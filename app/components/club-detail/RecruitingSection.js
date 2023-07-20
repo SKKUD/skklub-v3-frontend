e46a3f4b1a7935f79238eccb7dd0d79c03d54509
richcire <eric9907@naver.com>
2023-07-20 15:36:43 +0900

    redesign recruit section

--- app/components/club-detail/RecruitingSection.js
+++ app/components/club-detail/RecruitingSection.js
@@ -33,17 +33,92 @@ const RecruitInfoBox = styled.div`
   align-items: center;
 `;
 
+const RTable = styled.table`
+  padding-top: 4px;
+  border-collapse: separate;
+  border-spacing: 0;
+  width: 100%;
+`;
+
+const RThead = styled.thead``;
+
+const RTr = styled.tr`
+  &:first-of-type {
+    border-bottom-left-radius: 10px;
+  }
+  &:last-of-type {
+    border-bottom-right-radius: 10px;
+  }
+`;
+
+const RTh = styled.th`
+  border: 1px solid #fff;
+  padding-top: 14px;
+  padding-bottom: 14px;
+  color: #fff;
+  font-family: Pretendard;
+  font-size: 22px;
+  font-style: normal;
+  font-weight: 700;
+  line-height: 160%;
+  &:first-of-type {
+    border-top-left-radius: 10px;
+  }
+  &:last-of-type {
+    border-top-right-radius: 10px;
+  }
+`;
+
+const RTd = styled.td`
+  border: 1px solid #fff;
+  padding-top: 33px;
+  padding-bottom: 40px;
+  color: #fff;
+  text-align: center;
+  font-family: Pretendard;
+  font-size: 16px;
+  font-style: normal;
+  font-weight: 500;
+  line-height: 160%; /* 25.6px */
+
+  &:first-of-type {
+    border-bottom-left-radius: 10px;
+  }
+  &:last-of-type {
+    border-bottom-right-radius: 10px;
+  }
+`;
+
 export default function RecruitingSection() {
   return (
     <SectionWrapper
       style={{
         gridColumnStart: 1,
         gridColumnEnd: 4,
+        height: "307px",
       }}
     >
-      <SectionTitle>Recruiting</SectionTitle>
-      <SectionDivider />
-      <RecruitContentWrapper>
+      <SectionTitle>모집 정보</SectionTitle>
+      <RTable>
+        <RThead>
+          <RTr>
+            <RTh>모집 시기</RTh>
+            <RTh>모집 인원</RTh>
+            <RTh>모집 방식</RTh>
+            <RTh>활동 기간</RTh>
+          </RTr>
+        </RThead>
+        <tbody>
+          <RTr>
+            <RTd>asdf</RTd>
+            <RTd>asdf</RTd>
+            <RTd>asdf</RTd>
+            <RTd>asdf</RTd>
+          </RTr>
+        </tbody>
+      </RTable>
+      {/* <SectionDivider /> */}
+      {/* <RecruitContentWrapper>
         <RecruitContent>
           <RecruitSubTitle>모집 시기</RecruitSubTitle>
           <RecruitInfoBox>3.13(월) ~ 3.27(월)</RecruitInfoBox>
@@ -60,7 +135,7 @@ export default function RecruitingSection() {
           <RecruitSubTitle>모집 시기</RecruitSubTitle>
           <RecruitInfoBox>3.13(월) ~ 3.27(월)</RecruitInfoBox>
         </RecruitContent>
-      </RecruitContentWrapper>
+      </RecruitContentWrapper> */}
     </SectionWrapper>
   );
 }
