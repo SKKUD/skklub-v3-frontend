bf79260da316c08ab1bc5fb3bf5ebe3d99e311cd
richcire <eric9907@naver.com>
2023-07-26 19:11:57 +0900

    apply responsive design

--- app/components/club-detail/AboutSection.js
+++ app/components/club-detail/AboutSection.js
@@ -6,36 +6,13 @@ import { SectionDivider, SectionTitle } from "./common";
 const AboutContent = styled.div`
   height: 226px;
   word-break: keep-all;
-
   font-family: Pretendard;
   font-size: 16px;
   font-style: normal;
   font-weight: 500;
   line-height: 160%; /* 25.6px */
-`;
-
-const ClubPageNaviagateBtn = styled.button`
-  position: absolute;
-  bottom: 24px;
-  right: 30px;
-  width: 174px;
-  height: 51px;
-  background-color: #c4cbcd;
-  border-radius: 100px;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  font-weight: 700;
-  font-size: 1.25rem;
-  line-height: 107.5%;
-  color: white;
-  border: none;
-  @media (max-width: 760px) {
-    position: relative;
-    margin: 0 auto;
-    right: 0;
-    bottom: 0;
-    margin-top: 2rem;
+  @media (max-width: 425px) {
+    height: auto;
   }
 `;
 
