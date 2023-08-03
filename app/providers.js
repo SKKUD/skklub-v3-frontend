55f153782fc33807d864974eef60f2750f5d746d
richcire <eric9907@naver.com>
2023-08-03 13:26:07 +0900

    create recoil root provider

--- app/providers.js
+++ app/providers.js
@@ -5,11 +5,12 @@ import { CssBaseline, createTheme } from "@mui/material";
 import getDesignTokens from "@/utils/getDesignTokens";
 import { useState } from "react";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { RecoilRoot } from "recoil";
+import { RecoilRoot, useRecoilValue } from "recoil";
 import useThemeModeDetect from "@/hooks/useThemeModeDetect";
+import { isDarkModeState } from "@/utils/atoms";
 
 const Providers = ({ children }) => {
-  const isDarkMode = useThemeModeDetect();
+  const isDarkMode = useRecoilValue(isDarkModeState);
   const darkModeTheme = createTheme(getDesignTokens("dark"));
   const lightModeTheme = createTheme(getDesignTokens("light"));
   const [client] = useState(
@@ -18,9 +19,8 @@ const Providers = ({ children }) => {
   return (
     <ThemeProvider theme={isDarkMode ? darkModeTheme : lightModeTheme}>
       <CssBaseline />
-      <RecoilRoot>
-        <QueryClientProvider client={client}>{children}</QueryClientProvider>
-      </RecoilRoot>
+
+      <QueryClientProvider client={client}>{children}</QueryClientProvider>
     </ThemeProvider>
   );
 };
