cd72aba2fa92e6c055ee3ca845ce1bcce642314a
Haeun Oh <73781124+OHaEuN@users.noreply.github.com>
2023-08-09 23:51:16 +0900

    fix landing gif

--- app/page.js
+++ app/page.js
@@ -8,33 +8,15 @@ import LocationSelectBtn from "./components/start/LocationSelectBtn";
 import { useRecoilValue } from "recoil";
 import { isDarkModeState } from "@/utils/atoms";
 import darkWebGif from "@/public/assets/animations/web_loading_dark.gif";
-import darkWebLastframe from "@/public/assets/animations/web_dark_lastframe.png";
 import lightWebGif from "@/public/assets/animations/web_loading_light.gif";
-import lightWebLastframe from "@/public/assets/animations/web_light_lastframe.png";
 import darkMobGif from "@/public/assets/animations/mob_loading_dark.gif";
-import darkMobLastframe from "@/public/assets/animations/mob_dark_lastframe.png";
 import lightMobGif from "@/public/assets/animations/mob_loading_light.gif";
-import lightMobLastframe from "@/public/assets/animations/mob_light_lastframe.png";
-import { useEffect, useState } from "react";
 import { useMediaQuery } from "@mui/material";
 
 const StartPageWrapper = styled.div`
   width: 100vw;
   height: 100vh;
   position: relative;
-  /* background-image: ${(props) =>
-    props.isDarkMode
-      ? 'url("/assets/animations/web_loading.gif")'
-      : 'url("/assets/animations/web_loading_test.gif")'};
-  background-size: cover;
-  background-repeat: no-repeat;
-  background-position: center;
-  @media (max-width: 425px) {
-    background-image: ${(props) =>
-    props.isDarkMode
-      ? 'url("/assets/animations/mobile_loading.gif")'
-      : 'url("/assets/animations/mobile_loading_light.gif")'};
-  } */
   overflow: hidden;
 `;
 
@@ -73,6 +55,7 @@ const Phrase = styled.h2`
   line-height: normal;
   margin: 0;
   margin-bottom: 20px;
+  word-break: keep-all;
   @media (max-width: 425px) {
     font-size: 1.5rem;
   }
@@ -109,57 +92,18 @@ const BtnContainer = styled.div`
 
 export default function Home() {
   const isDarkMode = useRecoilValue(isDarkModeState);
-  const [playGif, setPlayGif] = useState(true);
   const match425 = useMediaQuery("(max-width:425px)");
-
-  useEffect(() => {
-    if (playGif) {
-      setTimeout(() => {
-        setPlayGif(false);
-      }, 1000); // 1초 후에 GIF 재생 멈추기
-    }
-  }, [playGif]);
-
   return (
     <StartPageWrapper isDarkMode={isDarkMode}>
-      {/* <VideoWrapper>
-        <BgVideo autoPlay muted>
-          <source
-            src={
-              isDarkMode
-                ? "/assets/animations/web_loading_dark.mkv"
-                : "/assets/animations/web_loading_light.mkv"
-            }
-            type="video/mp4"
-          />
-          Browser not supported
-        </BgVideo>
-      </VideoWrapper> */}
       {match425 ? (
         <Image
-          src={
-            playGif
-              ? isDarkMode
-                ? darkMobGif
-                : lightMobGif
-              : isDarkMode
-              ? darkMobLastframe
-              : lightMobLastframe
-          }
+          src={isDarkMode ? darkMobGif : lightMobGif}
           alt=""
           style={{ height: "100vh", width: "100vw" }}
         />
       ) : (
         <Image
-          src={
-            playGif
-              ? isDarkMode
-                ? darkWebGif
-                : lightWebGif
-              : isDarkMode
-              ? darkWebLastframe
-              : lightWebLastframe
-          }
+          src={isDarkMode ? darkWebGif : lightWebGif}
           alt=""
           style={{ height: "100vh", width: "100vw" }}
         />
